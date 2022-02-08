e792c75163c1679ca490f71b25209cf7a6cb8ff1
strugglebak <strugglebak@gmail.com>
2022-02-08 21:09:16 +0800

    feat: add named_return_values.go

---
+++ 2_basics/6_named_return_values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Naked return statements should be used only in short functions
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
