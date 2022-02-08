944179e0412a64bb41adcc4d2998f71fbfb10815
strugglebak <strugglebak@gmail.com>
2022-02-08 21:28:45 +0800

    feat: add zero values

---
+++ 2_basics/9_zero_values.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+zero value
+1. 0
+2. false
+3. ""
+*/
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Println("%v %v %v %q\n", i, f, b, s)
+}
