4d5976215a116b87d2d0ad16218b9f630e1939f8
strugglebak <strugglebak@gmail.com>
2022-02-09 18:35:58 +0800

    feat: add stacking defers

--- 2_basics/17_defer.go
+++ 2_basics/17_defer.go
@@ -8,4 +8,11 @@ func main() {
 	// "world" will be output until main returns
 	defer fmt.Println("world")
 	fmt.Println("hello")
+
+	fmt.Println("counting")
+	for i:= 0; i < 10; i++ {
+		// defer into a stack
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 }
