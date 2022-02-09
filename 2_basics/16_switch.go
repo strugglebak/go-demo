0bc333192885ec20197f55de716208778bc39b68
strugglebak <strugglebak@gmail.com>
2022-02-09 18:30:32 +0800

    feat: add switch with no condition

--- 2_basics/16_switch.go
+++ 2_basics/16_switch.go
@@ -29,4 +29,14 @@ func main() {
 		default:
 			fmt.Println("Too faraway.")
 	}
+
+	t := time.Now()
+	switch {
+		case t.Hour() < 12:
+			fmt.Println("Good morning!")
+		case t.Hour() < 17:
+			fmt.Println("Good afternoon.")
+		default:
+			fmt.Println("Good evening.")
+	}
 }
