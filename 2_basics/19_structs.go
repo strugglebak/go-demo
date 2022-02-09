74ab9af490e4f7479a20190beba52a71dc3e717f
strugglebak <strugglebak@gmail.com>
2022-02-09 18:45:49 +0800

    feat: add struct literals

--- 2_basics/19_structs.go
+++ 2_basics/19_structs.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 )
 
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
+
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
+
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	_p = &Vertex{1, 2}
+)
 
 func main() {
 	v := Vertex{1, 2}
@@ -19,4 +30,6 @@ func main() {
 	p := &v
 	p.X = 1e9
 	fmt.Println(v)
+
+	fmt.Println(v1, _p, v2, v3)
 }
