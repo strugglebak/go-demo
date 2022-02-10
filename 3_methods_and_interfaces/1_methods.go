d7ad14178dbf669bef3a93e8e367ca1593656aa0
strugglebak <strugglebak@gmail.com>
2022-02-10 15:58:41 +0800

    feat: add methods continued

--- 3_methods_and_interfaces/1_methods.go
+++ 3_methods_and_interfaces/1_methods.go
@@ -18,8 +18,20 @@ func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// declare a method on non-struct types
+type MyFloat float64
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
 	fmt.Println(Abs(v))
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
 }
