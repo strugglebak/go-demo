b1791aa0d622c6496432010c3cbd39bc59cf28a9
strugglebak <strugglebak@gmail.com>
2022-02-10 15:32:40 +0800

    feat: add function closures

--- 2_basics/25_function_values.go
+++ 2_basics/25_function_values.go
@@ -13,8 +13,24 @@ func main() {
 	fmt.Println(hypot(5, 12))
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2 * i),
+		)
+	}
 }
 
 func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
