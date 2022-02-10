ad23c855ce2484c79d5938faec435975551bdab5
strugglebak <strugglebak@gmail.com>
2022-02-10 16:46:00 +0800

    feat: add type switches

--- 3_methods_and_interfaces/3_type_assertions.go
+++ 3_methods_and_interfaces/3_type_assertions.go
@@ -26,4 +26,19 @@ func main() {
 	// panic: interface conversion: interface {} is string, not float64
 	// f = i.(float64)
 	// fmt.Println(f)
+
+	do(21)
+	do("hello")
+	do(true)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
 }
